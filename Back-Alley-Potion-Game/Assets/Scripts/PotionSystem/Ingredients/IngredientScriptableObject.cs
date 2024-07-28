cf4eff81afab77109f7c8a8adb06c17fcbd32137
Mia Kellett <71409898+MiaKellett02@users.noreply.github.com>
2024-07-29 00:03:39 +0100

    Ingredients, potions and potion recipes.

---
+++ Back-Alley-Potion-Game/Assets/Scripts/PotionSystem/Ingredients/IngredientScriptableObject.cs
@@ -0,0 +1,44 @@
+//////////////////////////////////////////////////////////////////////////////
+/// Filename: IngredientScriptableObject.cs
+/// Author: Mia Kellett
+/// Date Created: 28/07/2024
+/// Purpose: To store ingredient data.
+//////////////////////////////////////////////////////////////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "PotionGame/Ingredient")]
+public class IngredientScriptableObject : ScriptableObject {
+	//Variables.
+	[SerializeField] private string m_ingredientName;
+	[SerializeField] private Texture2D m_ingredientVisual;
+
+	//Functions.
+	public string GetIngredientName() {
+		return m_ingredientName;
+	}
+
+	public Texture2D GetIngredientVisual() {
+		return m_ingredientVisual;
+	}
+
+	//Unity Functions.
+	private void OnEnable() {
+		//Get the name of the ingredient.
+		string path = AssetDatabase.GetAssetPath(this);
+		string filename = System.IO.Path.GetFileName(path);
+		m_ingredientName = filename.Split(".")[0];
+		Debug.Log("Filename: " + filename);
+	}
+
+	private void OnValidate() {
+		//Get the name of the ingredient.
+		string path = AssetDatabase.GetAssetPath(this);
+		string filename = System.IO.Path.GetFileName(path);
+		m_ingredientName = filename.Split(".")[0];
+		Debug.Log("Filename: " + filename);
+	}
+}
---
+++ Back-Alley-Potion-Game/Assets/Scripts/PotionSystem/Potions/PotionScriptableObject.cs
@@ -0,0 +1,54 @@
+////////////////////////////////////////////////////////////////////////////////
+/// Filename: PotionScriptableObject.cs
+/// Author: Mia Kellett
+/// Date Created: 28/07/2024
+/// Purpose: To hold potion data and the crafting recipe for that potion.
+////////////////////////////////////////////////////////////////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "PotionGame/Potion")]
+public class PotionScriptableObject : ScriptableObject {
+	//Variables.
+	[SerializeField] private string m_potionName;
+	[SerializeField][TextArea()] private string m_description;
+	[SerializeField] private Texture2D m_potionVisual;
+	[SerializeField] private IngredientScriptableObject[] m_potionIngredients;
+
+	//Functions.
+	public string GetPotionName() {
+		return m_potionName;
+	}
+
+	public string GetPotionDescription() {
+		return m_description;
+	}
+
+	public Texture2D GetPotionVisual() {
+		return m_potionVisual;
+	}
+
+	public IngredientScriptableObject[] GetPotionIngredients() {
+		return m_potionIngredients;
+	}
+
+	//Unity Functions.
+	private void OnEnable() {
+		//Get the name of the ingredient.
+		string path = AssetDatabase.GetAssetPath(this);
+		string filename = System.IO.Path.GetFileName(path);
+		m_potionName = filename.Split(".")[0];
+		Debug.Log("Filename: " + filename);
+	}
+
+	private void OnValidate() {
+		//Get the name of the ingredient.
+		string path = AssetDatabase.GetAssetPath(this);
+		string filename = System.IO.Path.GetFileName(path);
+		m_potionName = filename.Split(".")[0];
+		Debug.Log("Filename: " + filename);
+	}
+}
