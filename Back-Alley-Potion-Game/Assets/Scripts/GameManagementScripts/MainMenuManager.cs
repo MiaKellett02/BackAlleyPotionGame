75274668fd56bc3ac416b06cfc4548e1ba43bd2c
Michelle B <axew13mail@gmail.com>
2024-07-31 00:15:09 +0100

    Finish main menu

--- Back-Alley-Potion-Game/Assets/Scripts/GameManagementScripts/MainMenuManager.cs
+++ Back-Alley-Potion-Game/Assets/Scripts/GameManagementScripts/MainMenuManager.cs
@@ -7,8 +7,20 @@ public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private float m_fadeTime = 1.0f;
 
+    [SerializeField] private GameObject mainButtonsHolder;
+    [SerializeField] private GameObject attributionsPanel;
+
     [SerializeField] private Animator cameraAnimator;
     [SerializeField] private AnimationClip zoomIntoShopClip;
+
+    [SerializeField] private AudioSource door;
+    [SerializeField] private AudioClip doorClip;
+
+    public void Start()
+    {
+        // Limit to 60 fps as any more is unnecessary and may cause performance issues
+        Application.targetFrameRate = 60;
+    }
 
     public void PlayGame ()
     {
@@ -17,9 +29,25 @@ public class MainMenuManager : MonoBehaviour
         StartCoroutine(FadeToSceneAfterAnimation());
     }
 
+    public void OpenAttributions ()
+    {
+        mainButtonsHolder.SetActive(false);
+        attributionsPanel.SetActive(true);
+    }
+
+    public void CloseAttributions()
+    {
+        mainButtonsHolder.SetActive(true);
+        attributionsPanel.SetActive(false);
+    }
+
     private IEnumerator FadeToSceneAfterAnimation()
     {
-        yield return new WaitForSeconds(zoomIntoShopClip.length);
+        float timeUntilFade = (zoomIntoShopClip.length / 3) * 2;
+
+        yield return new WaitForSeconds(timeUntilFade);
+
+        door.PlayOneShot(doorClip);
 
         FadeUI.Instance.FadeOut(m_fadeTime, () => { SceneManager.LoadScene(1); });
     }
