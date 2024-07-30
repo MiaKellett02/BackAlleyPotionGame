085ae8f2c23d83aabbe7fdaa66b13789045839c1
Mia Kellett <71409898+MiaKellett02@users.noreply.github.com>
2024-07-30 20:26:46 +0100

    Recipe book and fade ui

---
+++ Back-Alley-Potion-Game/Assets/Scripts/PotionSystem/Crafting/RecipeBookUI.cs
@@ -0,0 +1,62 @@
+///////////////////////////////////////////////////////////
+/// Filename: RecipeBookUI.cs
+/// Author: Mia Kellett
+/// Date Created: 30/07/2024
+/// Purpose: To Display recipes.
+///////////////////////////////////////////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecipeBookUI : MonoBehaviour {
+	//Variables to assign via the unity inspector.
+	[SerializeField] private Button m_leftButton;
+	[SerializeField] private Button m_rightButton;
+	[SerializeField] private List<PotionScriptableObject> m_potions;
+	[Header("Images")]
+	[SerializeField] private Image m_ingredientOne;
+	[SerializeField] private Image m_ingredientTwo;
+	[SerializeField] private Image m_ingredientThree;
+	[SerializeField] private Image m_potionImage;
+
+	//Private Variables.
+	private int m_currentIndex = 0;
+
+	//Unity Functions.
+	private void Awake() {
+
+		UpdateUI();
+	}
+
+	private void Start() {
+		m_leftButton.onClick.AddListener(() => {
+			m_currentIndex--;
+			m_currentIndex = Mathf.Clamp(m_currentIndex, 0, m_potions.Count - 1);
+			UpdateUI();
+		});
+
+		m_rightButton.onClick.AddListener(() => {
+			m_currentIndex++;
+			m_currentIndex = Mathf.Clamp(m_currentIndex, 0, m_potions.Count - 1);
+			UpdateUI();
+		});
+
+		UpdateUI();
+		this.gameObject.SetActive(false);
+	}
+
+	//Private Functions.
+	private void UpdateUI() {
+		PotionScriptableObject currentPotion = m_potions[m_currentIndex];
+		IngredientScriptableObject[] ingredients = currentPotion.GetPotionIngredients();
+
+		//Update ingredients.
+		m_ingredientOne.sprite = ingredients[0].GetIngredientVisual();
+		m_ingredientTwo.sprite = ingredients[1].GetIngredientVisual();
+		m_ingredientThree.sprite = ingredients[2].GetIngredientVisual();
+
+		//Update output potion
+		m_potionImage.sprite = currentPotion.GetPotionVisual();
+	}
+}
